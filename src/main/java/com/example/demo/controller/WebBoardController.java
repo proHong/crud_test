ba1dea8a52e38f1f8341b514985c852aea4978aa
Hong <tjdgus0454@gmail.com>
2020-04-20 20:16:55 +0900

    add PageVO

--- src/main/java/com/example/demo/controller/WebBoardController.java
+++ src/main/java/com/example/demo/controller/WebBoardController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.vo.PageVO;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,8 @@ import lombok.extern.java.Log;
 public class WebBoardController {
 
     @GetMapping("/list")
-    public void list(){
-        log.info("list() called...");
+    public void list(PageVO vo){
+        Pageable page = vo.makePageable(0, "bno");
+        log.info("" + page);
     }
 }
