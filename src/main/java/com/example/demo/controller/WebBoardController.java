d532bdbe44d75c8ecbed5b3ea3bbc60107c600b1
Hong <tjdgus0454@gmail.com>
2020-04-21 02:51:12 +0900

    add registerBoard

--- src/main/java/com/example/demo/controller/WebBoardController.java
+++ src/main/java/com/example/demo/controller/WebBoardController.java
@@ -12,9 +12,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/boards/")
@@ -36,4 +40,22 @@ public class WebBoardController {
 
         model.addAttribute("result", new PageMaker(result));
     }
+
+    @GetMapping("/register")
+    public void registerGET(@ModelAttribute("vo") WebBoard vo){
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr){
+
+        log.info("register post");
+        log.info("" + vo);
+
+        repo.save(vo);
+        rttr.addFlashAttribute("msg", "success");
+
+        return "redirect:/boards/list";
+    }
+    
 }
