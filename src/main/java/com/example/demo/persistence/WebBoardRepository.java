aa9ae989807738c79cdf3f7bc8fd29bf23544887
Hong <tjdgus0454@gmail.com>
2020-04-20 19:54:24 +0900

    addPageTest

--- src/main/java/com/example/demo/persistence/WebBoardRepository.java
+++ src/main/java/com/example/demo/persistence/WebBoardRepository.java
@@ -1,9 +1,27 @@
 package com.example.demo.persistence;
 
+import com.example.demo.domain.QWebBoard;
 import com.example.demo.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface WebBoardRepository extends CrudRepository<WebBoard,Long> {
+public interface WebBoardRepository extends CrudRepository<WebBoard,Long>, QuerydslPredicateExecutor<WebBoard> {
+
+    public default Predicate makePredicate(String type, String keyword){
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        QWebBoard board = QWebBoard.webBoard;
+
+        // type if ~ else
+
+        // bno > 0
+        builder.and(board.bno.gt(0));
+
+        return builder;
+    }
 
 }
--- src/test/java/com/example/demo/WebBoardRepositoryTests.java
+++ src/test/java/com/example/demo/WebBoardRepositoryTests.java
@@ -9,10 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.java.Log;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Log
 public class WebBoardRepositoryTests {
 
     @Autowired
@@ -30,5 +37,19 @@ public class WebBoardRepositoryTests {
 
                 repo.save(board);
             });
+        }
+
+    @Test
+        public void testList1(){
+
+            Pageable pageable = PageRequest.of(0, 20, Direction.DESC , "bno");
+
+            Page<WebBoard> result = repo.findAll(
+                repo.makePredicate(null, null), pageable);
+
+            log.info("Page: " + result.getPageable());
+
+            log.info("-----------------------");
+            result.getContent().forEach(board -> log.info("" + board));
         }
 }
